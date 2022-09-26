4c4812df71958bb85234894db13ca88fa191f6b7
furstFighter <114370207+furstFighter@users.noreply.github.com>
2022-09-26 03:33:42 +0100

    add image prefetch for faster loading

--- main.js
+++ main.js
@@ -1,67 +1,74 @@
+const prefetchCount = 2;
+const prefetch = { nsfw: [], sfw: [] };
+const NSFWDiv = document.querySelector("#nsfw"),
+	  SFWDiv = document.querySelector("#sfw"),
+	  NSFWOptions = document.querySelector("#nsfw-select"),
+	  SFWOptions = document.querySelector("#sfw-select"),
+	  image = document.querySelector("#waifu-image");
 
-const randomImage= document.querySelector(".random-image"),
-	  randomWaifu = document.querySelector(".random-waifu"),
-	  randomWaifuNsfw = document.querySelector(".random-waifu-nsfw"),
-	  formSelect = document.querySelector(".form-select"),
-	  formSelectNsfw = document.querySelector(".form-select-nsfw"),
-	  nsfwToggle = document.querySelector("#nsfwToggle"),
-	  imageWrapper = document.querySelector('.image-wrapper')
-document.querySelector(".sfw").style.display = "flex"
-document.querySelector(".nsfw").style.display = "none"
+/* Detecting a change for NSFW selections */
+const isNSFW = document.querySelector('input[type=checkbox]');    
+function updateOptions() {
+	NSFWDiv.style.display = isNSFW.checked ? "inline-block" : "none"
+	SFWDiv.style.display = isNSFW.checked ? "none" : "inline-block"
 
-formSelect.addEventListener('change', generateWaifu)
-formSelectNsfw.addEventListener('change', generateWaifu)
-
-let checkboxNsfw = document.querySelector('input[type=checkbox]');    
-
-nsfwToggle.addEventListener('change', ()=>{
-	if (checkboxNsfw.checked) {
-		document.querySelector(".nsfw").style.display = "flex"
-		document.querySelector(".sfw").style.display = "none"
-		generateWaifu()
-	} else {
-		document.querySelector(".sfw").style.display = "flex"
-		document.querySelector(".nsfw").style.display = "none"
-		generateWaifu()
-	}
-	
-})
-
-const loading = './load.svg'
-randomImage.src = loading
-
-randomWaifu.addEventListener("click",generateWaifu);
-randomWaifuNsfw.addEventListener("click",generateWaifu)
-
-function generateWaifu() {
-	randomImage.src = loading
-	if (document.querySelector(".sfw").style.display == "flex") {
-		fetchWaifu()
-	} else if (document.querySelector(".nsfw").style.display == "flex") {
-		fetchWaifuNsfw()
-	}
-
-
+	generateWaifu();
 }
 
+/* Fetches an image from both apis */
 async function fetchWaifu() {
-	let category = formSelect.value;
-
-	await fetch(`https://api.waifu.pics/sfw/${category}`)
+	/* Fetching nsfw */
+	return [
+		(await fetch(`https://api.waifu.pics/nsfw/${NSFWOptions.value}`)
 		.then(response => response.json())
-		.then(quote =>  {
-			randomImage.src = quote.url
-	})
+		.then(query => query.url)),
+	
+		/* Fetching sfw */
+		(await fetch(`https://api.waifu.pics/sfw/${SFWOptions.value}`)
+			.then(response => response.json())
+			.then(query => query.url))
+	]
 }
 
-async function fetchWaifuNsfw() {
-	let category = formSelectNsfw.value;
-	await fetch(`https://api.waifu.pics/nsfw/${category}`)
-		.then(response => response.json())
-		.then(quote =>  {
-			randomImage.src = quote.url
-	})
+/* Creates an image in the background to load a src */
+function createPrefetchImages() {
+	const prefetchImg = new Image();
+	prefetchImg.style = "display: none;"
+	prefetchImg.src = ""
+	
+	for (let i = 0; i < prefetchCount; i++) {
+		prefetch.nsfw.push(prefetchImg.cloneNode());
+		prefetch.sfw.push(prefetchImg.cloneNode());
+		document.body.appendChild(prefetch.nsfw[i]);
+		document.body.appendChild(prefetch.sfw[i]);
+	}
 }
 
+/* Fetches multiple as a 'prefetch' so it can load faster */
+async function prefetchWaifu() {
+	const nsfwFirst = prefetch.nsfw.shift();
+	const sfwFirst = prefetch.sfw.shift()	
+	const [nsfwSrc, sfwSrc] = await fetchWaifu();
 
-generateWaifu()
+	nsfwFirst.src = nsfwSrc;
+	sfwFirst.src = sfwSrc;
+	prefetch.nsfw.push(nsfwFirst);
+	prefetch.sfw.push(sfwFirst);
+}
+
+/* Gets an image and sets it as the src */
+let previousCategory = undefined;
+async function generateWaifu() {
+	if (previousCategory != (isNSFW.checked ? NSFWOptions.value : SFWOptions.value).toString()) {
+		for (let i = 0; i < prefetchCount; i++) { await prefetchWaifu() }
+		previousCategory = (isNSFW.checked ? NSFWOptions.value : SFWOptions.value).toString()
+	} else {
+		prefetchWaifu();
+	}
+
+	image.src = isNSFW.checked ? prefetch.nsfw[0].src : prefetch.sfw[0].src;
+}
+
+/* Initial setup */
+createPrefetchImages();
+updateOptions();
